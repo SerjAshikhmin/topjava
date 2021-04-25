b14dad3b11e295af7c76edb29f4921879aaacc67
sergey_ashikhmin <serashihmin@yandex.ru>
2021-04-25 12:56:25 +0300

    Revert "10 04 opt json view"
    This reverts commit 18ae0195

--- src/main/java/ru/javawebinar/topjava/model/Meal.java
+++ src/main/java/ru/javawebinar/topjava/model/Meal.java
@@ -1,11 +1,7 @@
 package ru.javawebinar.topjava.model;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonGetter;
-import com.fasterxml.jackson.annotation.JsonView;
 import org.hibernate.validator.constraints.Range;
 import org.springframework.format.annotation.DateTimeFormat;
-import ru.javawebinar.topjava.View;
 import ru.javawebinar.topjava.util.DateTimeUtil;
 
 import javax.persistence.*;
@@ -33,7 +29,7 @@ public class Meal extends AbstractBaseEntity {
 
     @Column(name = "date_time", nullable = false)
     @NotNull
-    @JsonView(View.JsonREST.class)
+    @DateTimeFormat(pattern = DateTimeUtil.DATE_TIME_PATTERN)
     private LocalDateTime dateTime;
 
     @Column(name = "description", nullable = false)
@@ -102,19 +98,6 @@ public class Meal extends AbstractBaseEntity {
 
     public void setUser(User user) {
         this.user = user;
-    }
-
-
-    @JsonGetter
-    @JsonView(View.JsonUI.class)
-    @JsonFormat(pattern = DateTimeUtil.DATE_TIME_PATTERN)
-    public LocalDateTime getDateTimeUI() {
-        return dateTime;
-    }
-
-    @DateTimeFormat(pattern = DateTimeUtil.DATE_TIME_PATTERN)
-    public void setDateTimeUI(LocalDateTime dateTime) {
-        this.dateTime = dateTime;
     }
 
     @Override
