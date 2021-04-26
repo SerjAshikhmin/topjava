5b63469074cf79e7aeb8a88007b7537a7cfd00a9
sergey_ashikhmin <serashihmin@yandex.ru>
2021-04-26 12:25:24 +0300

    10 15 csrf

--- src/main/webapp/resources/js/topjava.common.js
+++ src/main/webapp/resources/js/topjava.common.js
@@ -21,6 +21,12 @@ function makeEditable(ctx) {
     });
 
     $.ajaxSetup({cache: false});
+
+    var token = $("meta[name='_csrf']").attr("content");
+    var header = $("meta[name='_csrf_header']").attr("content");
+    $(document).ajaxSend(function (e, xhr, options) {
+        xhr.setRequestHeader(header, token);
+    });
 }
 
 function add() {
